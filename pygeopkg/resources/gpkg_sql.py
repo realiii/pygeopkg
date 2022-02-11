7b9828fd12ba33a38c39d83ba0d1cec09b1a3687
Jason Humber <jlhumber@openiii.com>
2022-02-11 08:54:49 -0600

    reflow SQL (remove parens and use """ instead)

--- pygeopkg/resources/gpkg_sql.py
+++ pygeopkg/resources/gpkg_sql.py
@@ -2,89 +2,77 @@
 SQL Statements to create an empty geopackage
 """
 
-GPKG_PRAGMA_APPLICATION_ID = (
-"""
-PRAGMA application_id=0x47504b47     
-"""
-)
+GPKG_PRAGMA_APPLICATION_ID = """PRAGMA application_id=0x47504b47"""
+GPKG_PRAGMA_USER_VERSION = """PRAGMA user_version=10200"""
 
-GPKG_PRAGMA_USER_VERSION = (
-"""
-PRAGMA user_version=10200
-"""
-)
 
-GPKG_SPATIAL_REF_SYS_TABLE = (
+GPKG_SPATIAL_REF_SYS_TABLE = """
+    CREATE TABLE gpkg_spatial_ref_sys (
+      srs_name TEXT NOT NULL,
+      srs_id INTEGER NOT NULL PRIMARY KEY,
+      organization TEXT NOT NULL,
+      organization_coordsys_id INTEGER NOT NULL,
+      definition  TEXT NOT NULL,
+      description TEXT)
 """
-CREATE TABLE gpkg_spatial_ref_sys (
-  srs_name TEXT NOT NULL,
-  srs_id INTEGER NOT NULL PRIMARY KEY,
-  organization TEXT NOT NULL,
-  organization_coordsys_id INTEGER NOT NULL,
-  definition  TEXT NOT NULL,
-  description TEXT)
-""")
 
-GPKG_CONTENTS_TABLE = (
+
+GPKG_CONTENTS_TABLE = """
+    CREATE TABLE gpkg_contents (
+      table_name TEXT NOT NULL PRIMARY KEY,
+      data_type TEXT NOT NULL,
+      identifier TEXT UNIQUE,
+      description TEXT DEFAULT '',
+      last_change DATETIME NOT NULL DEFAULT (strftime('%Y-%m-%dT%H:%M:%S.%fZ','now')),
+      min_x DOUBLE,
+      min_y DOUBLE,
+      max_x DOUBLE,
+      max_y DOUBLE,
+      srs_id INTEGER,
+      CONSTRAINT fk_gc_r_srs_id FOREIGN KEY (srs_id) REFERENCES gpkg_spatial_ref_sys(srs_id))
 """
-CREATE TABLE gpkg_contents (
-  table_name TEXT NOT NULL PRIMARY KEY,
-  data_type TEXT NOT NULL,
-  identifier TEXT UNIQUE,
-  description TEXT DEFAULT '',
-  last_change DATETIME NOT NULL DEFAULT (strftime('%Y-%m-%dT%H:%M:%S.%fZ','now')),
-  min_x DOUBLE,
-  min_y DOUBLE,
-  max_x DOUBLE,
-  max_y DOUBLE,
-  srs_id INTEGER,
-  CONSTRAINT fk_gc_r_srs_id FOREIGN KEY (srs_id) REFERENCES gpkg_spatial_ref_sys(srs_id))
-""")
 
-GPKG_GEOMETRY_COLUMNS_TABLE = (
+
+GPKG_GEOMETRY_COLUMNS_TABLE = """
+    CREATE TABLE gpkg_geometry_columns (
+      table_name TEXT NOT NULL,
+      column_name TEXT NOT NULL,
+      geometry_type_name TEXT NOT NULL,
+      srs_id INTEGER NOT NULL,
+      z TINYINT NOT NULL,
+      m TINYINT NOT NULL,
+      CONSTRAINT pk_geom_cols PRIMARY KEY (table_name, column_name),
+      CONSTRAINT uk_gc_table_name UNIQUE (table_name),
+      CONSTRAINT fk_gc_tn FOREIGN KEY (table_name) REFERENCES gpkg_contents(table_name),
+      CONSTRAINT fk_gc_srs FOREIGN KEY (srs_id) REFERENCES gpkg_spatial_ref_sys (srs_id))
 """
-CREATE TABLE gpkg_geometry_columns (
-  table_name TEXT NOT NULL,
-  column_name TEXT NOT NULL,
-  geometry_type_name TEXT NOT NULL,
-  srs_id INTEGER NOT NULL,
-  z TINYINT NOT NULL,
-  m TINYINT NOT NULL,
-  CONSTRAINT pk_geom_cols PRIMARY KEY (table_name, column_name),
-  CONSTRAINT uk_gc_table_name UNIQUE (table_name),
-  CONSTRAINT fk_gc_tn FOREIGN KEY (table_name) REFERENCES gpkg_contents(table_name),
-  CONSTRAINT fk_gc_srs FOREIGN KEY (srs_id) REFERENCES gpkg_spatial_ref_sys (srs_id))
-""")
 
-GPKG_TILE_MATRIX_SET_TABLE = (
+
+GPKG_TILE_MATRIX_SET_TABLE = """
+    CREATE TABLE gpkg_tile_matrix_set (
+      table_name TEXT NOT NULL PRIMARY KEY,
+      srs_id INTEGER NOT NULL,
+      min_x DOUBLE NOT NULL,
+      min_y DOUBLE NOT NULL,
+      max_x DOUBLE NOT NULL,
+      max_y DOUBLE NOT NULL,
+      CONSTRAINT fk_gtms_table_name FOREIGN KEY (table_name) REFERENCES gpkg_contents(table_name),
+      CONSTRAINT fk_gtms_srs FOREIGN KEY (srs_id) REFERENCES gpkg_spatial_ref_sys (srs_id))
 """
-CREATE TABLE gpkg_tile_matrix_set (
-  table_name TEXT NOT NULL PRIMARY KEY,
-  srs_id INTEGER NOT NULL,
-  min_x DOUBLE NOT NULL,
-  min_y DOUBLE NOT NULL,
-  max_x DOUBLE NOT NULL,
-  max_y DOUBLE NOT NULL,
-  CONSTRAINT fk_gtms_table_name FOREIGN KEY (table_name) REFERENCES gpkg_contents(table_name),
-  CONSTRAINT fk_gtms_srs FOREIGN KEY (srs_id) REFERENCES gpkg_spatial_ref_sys (srs_id))
-""")
 
-GPKG_TILE_MATRIX_TABLE = (
-"""
-CREATE TABLE gpkg_tile_matrix (
-  table_name TEXT NOT NULL,
-  zoom_level INTEGER NOT NULL,
-  matrix_width INTEGER NOT NULL,
-  matrix_height INTEGER NOT NULL,
-  tile_width INTEGER NOT NULL,
-  tile_height INTEGER NOT NULL,
-  pixel_x_size DOUBLE NOT NULL,
-  pixel_y_size DOUBLE NOT NULL,
-  CONSTRAINT pk_ttm PRIMARY KEY (table_name, zoom_level),
-  CONSTRAINT fk_tmm_table_name FOREIGN KEY (table_name) REFERENCES gpkg_contents(table_name))
-""")
 
-GPKG_EXTENSIONS = (
+GPKG_TILE_MATRIX_TABLE = """
+    CREATE TABLE gpkg_tile_matrix (
+      table_name TEXT NOT NULL,
+      zoom_level INTEGER NOT NULL,
+      matrix_width INTEGER NOT NULL,
+      matrix_height INTEGER NOT NULL,
+      tile_width INTEGER NOT NULL,
+      tile_height INTEGER NOT NULL,
+      pixel_x_size DOUBLE NOT NULL,
+      pixel_y_size DOUBLE NOT NULL,
+      CONSTRAINT pk_ttm PRIMARY KEY (table_name, zoom_level),
+      CONSTRAINT fk_tmm_table_name FOREIGN KEY (table_name) REFERENCES gpkg_contents(table_name))
 """
 CREATE TABLE gpkg_extensions (
   table_name TEXT,
